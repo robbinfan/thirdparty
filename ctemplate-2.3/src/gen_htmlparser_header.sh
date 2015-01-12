60160ef4260ac9c9f79d4f805f3e42c23e756c79
robbinfan <fanyu83@gmail.com>
2015-01-12 07:53:39 +0000

    add ctemplate 2.3

---
+++ ctemplate-2.3/src/gen_htmlparser_header.sh
@@ -0,0 +1,3 @@
+./htmlparser/generate_fsm.py ./htmlparser/htmlparser_fsm.config > htmlparser/htmlparser_fsm.h
+./htmlparser/generate_fsm.py ./htmlparser/jsparser_fsm.config > htmlparser/jsparser_fsm.h
+./htmlparser/generate_fsm.py ./tests/statemachine_test_fsm.config > tests/statemachine_test_fsm.h
